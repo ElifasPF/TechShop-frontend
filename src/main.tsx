36a1fcb4270a348e277d6a1d0c7786a05f534398
ElifasPF <elifas.faustino@estudante.ifms.edu.br>
2025-11-08 15:41:15 -0400

    importação corretas de todas as rotas

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,10 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './componentes/login/login.tsx'
-import Cadastrar from './componentes/cadastro/cadastrar.tsx'
-import Carrinho from './componentes/carrinho/carrinho.tsx'
+import Cadastrar from './componentes/cadastro/cadastrar.tsx' // (Rota que já arrumamos)
+
+// 1. ESTA LINHA ESTAVA FALTANDO
+import Carrinho from './componentes/carrinho/Carrinho.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
